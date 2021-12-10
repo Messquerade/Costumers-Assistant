bd6e36ff29a0506cdecdf71c7ae76aa6df24dea5
Messquerade <annac.klingberg@gmail.com>
2021-12-10 14:31:23 -0800

    add middleware to authorize routes

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
 
 // @route GET api/auth
 // @description Test router
 // @access Public
-router.get('/', (req, res) => res.send('Auth route'));
+router.get('/', auth, (req, res) => res.send('Auth route'));
 
 module.exports = router;
