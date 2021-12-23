cca8e699a32320ad6eb107aa8c1c8c651786a2d7
Messquerade <annac.klingberg@gmail.com>
2021-12-23 10:21:41 -0800

    add login form

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,10 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link, Navigate } from 'react-router-dom';
+import { Container, FormGroup} from '@mui/material';
+
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  });
+
+  const { email, password } = formData;
+
+  const onChange = (e) => setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    console.log('Success');
+  }
+
+  
+
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <Container>
+      <h1>Sign In</h1>
+      <p>
+         Login To Your Account
+      </p>
+      <form  onSubmit={onSubmit}>
+        <FormGroup>
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={onChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password}
+            onChange={onChange}
+          />
+        </FormGroup>
+        <input type="submit" value="Login" />
+      </form>
+      <p>
+        Don't have an account? <Link to='/register'>Register</Link>
+      </p>
+    </Container>
   )
 }
 
