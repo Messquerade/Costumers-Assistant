46c8bc5a0d9caca9d0f53dace3286cce8e73da7b
Messquerade <annac.klingberg@gmail.com>
2021-12-23 10:05:50 -0800

    add register form

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,69 +1,86 @@
 import React, { useState } from 'react';
 import { Link, Navigate } from 'react-router-dom';
+import { Container, FormGroup} from '@mui/material';
 
 
 const Register = () => {
-  // const [formData, setFormData] = useState({
-  //   name: '',
-  //   email: '',
-  //   password: '',
-  //   password2: ''
-  // });
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  const onChange = (e) => setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    if (password !== password2) {
+      // setAlert('Passwords do not match', 'danger');
+      console.log('Passwords do not match');
+    } else {
+      // register({name, email, password});
+      console.log(formData);
+    }
+  }
+
+  
 
   return (
-    <h1>Register</h1>
-    // <section className="container">
-    //   <h1 className="large text-primary">Sign Up</h1>
-    //   <p className="lead">
-    //     <i className="fas fa-user" /> Create Your Account
-    //   </p>
-    //   <form className="form" onSubmit={onSubmit}>
-    //     <div className="form-group">
-    //       <input
-    //         type="text"
-    //         placeholder="Name"
-    //         name="name"
-    //         value={name}
-    //         onChange={onChange}
-    //       />
-    //     </div>
-    //     <div className="form-group">
-    //       <input
-    //         type="email"
-    //         placeholder="Email Address"
-    //         name="email"
-    //         value={email}
-    //         onChange={onChange}
-    //       />
-    //       <small className="form-text">
-    //         This site uses Gravatar so if you want a profile image, use a
-    //         Gravatar email
-    //       </small>
-    //     </div>
-    //     <div className="form-group">
-    //       <input
-    //         type="password"
-    //         placeholder="Password"
-    //         name="password"
-    //         value={password}
-    //         onChange={onChange}
-    //       />
-    //     </div>
-    //     <div className="form-group">
-    //       <input
-    //         type="password"
-    //         placeholder="Confirm Password"
-    //         name="password2"
-    //         value={password2}
-    //         onChange={onChange}
-    //       />
-    //     </div>
-    //     <input type="submit" className="btn btn-primary" value="Register" />
-    //   </form>
-    //   <p className="my-1">
-    //     Already have an account? <Link to="/login">Sign In</Link>
-    //   </p>
-    // </section>
+    <Container>
+      <h1>Sign Up</h1>
+      <p>
+         Create Your Account
+      </p>
+      <form  onSubmit={onSubmit}>
+        <FormGroup>
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            value={name}
+            onChange={onChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={onChange}
+          />
+          <small className="form-text">
+            This site uses Gravatar so if you want a profile image, use a
+            Gravatar email
+          </small>
+        </FormGroup>
+        <FormGroup>
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password}
+            onChange={onChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="password2"
+            value={password2}
+            onChange={onChange}
+          />
+        </FormGroup>
+        <input type="submit" value="Register" />
+      </form>
+      <p>
+        Already have an account? <Link to="/login">Sign In</Link>
+      </p>
+    </Container>
   )
 }
 
